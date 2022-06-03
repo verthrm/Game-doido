729fc7b4df18f09f18ef6308d35f8a1f244e1310
verthrm <92987438+verthrm@users.noreply.github.com>
2022-06-03 20:37:50 -0300

    Final
    Final

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 const gabigol = document.querySelector('.gabigol')
 const mesa = document.querySelector('.mesa')
+const p$ = document.getElementById("contador");
+
+let contador = 0;
+p$.innerHTML = contador
+const lop = setInterval(() => {
+    contador++;
+    p$.innerHTML = contador
+},1500);
 
 const jump = () => {
     gabigol.classList.add('jump');
-
     setTimeout(() => {
-
         gabigol.classList.remove('jump');
-
     }, 800)
 }
-
 const loop = setInterval(() => {
-
     const mesaPos = mesa.offsetLeft;
     const gabiAlt = +window.getComputedStyle(gabigol).bottom.replace('px', '')
 
     if (mesaPos < 130 && mesaPos > 0 && gabiAlt < 80) {
-
         mesa.style.animation= 'none'; 
         mesa.style.left = `${mesaPos}px`;
-
-
         gabigol.style.animation= 'none'; 
         gabigol.style.bottom = `${gabiAlt}px`;
-    }
-
+        gabigol.src = "./img/gg-ll.png";   
+        alert(contador);
+    } 
 }, 10);
 
-document.addEventListener('keydown', jump)
+document.addEventListener('keydown', jump);
